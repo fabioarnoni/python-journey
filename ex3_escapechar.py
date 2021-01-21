fc5627d80119d7fdb9f00e4ae8a046b95d708c1f
Fabio Arnoni <fabioarnoni@gmail.com>
2021-01-21 08:45:39 -0300

    Exercises - 01/21/2021

---
+++ ex3_escapechar.py
@@ -0,0 +1,28 @@
+splitString = "This string \n has been split over\n several lines"
+
+print(splitString)
+
+tabbedString = "1\t2\t3\t4\t5\t6"
+
+print(tabbedString)
+
+print('The pet shop owner said "No, no \'e\'s uh, ... He \'s resting".')
+print("The pet shop owner said \"No, no 'e's uh, ... He 's resting\".")
+print("""The pet shop owner said "No, no 'e's uh, ... He 's resting". """)
+
+anotherSplitString = """This string has been
+has been split over
+several lines"""
+
+print(anotherSplitString)
+
+
+notSplitedString = """This string has NOT been \
+splited over \
+several lines"""
+
+print(notSplitedString)
+
+#Scaping Back slashes
+print("C:\\Users\\timbuchalka\\notes")
+print(r"C:\Users\timbuchalka\notes")
---
+++ ex4_variables_and_types.py
@@ -0,0 +1,17 @@
+greetings = "Hello"
+name = "Fabio"
+
+age = 36
+print(greetings + " " + name + "." + "Your age is " + str(age) + " years")
+
+print(type(name))
+print(type(age))
+
+age = "36 years"
+
+print(age)
+print(type(age))
+
+age_in_words = "3 years"
+
+print(greetings + " " + name + "." + "Your age is " + age)
