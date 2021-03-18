c8757f1c45a498d97bc4c2e661870db7dcbca5e9
Fabio Arnoni <fabioarnoni@gmail.com>
2021-03-18 08:38:51 -0300

    Exercises - 03/18/2021

--- ex38_enumerate_function.py
+++ ex38_enumerate_function.py
@@ -4,10 +4,21 @@ computer_parts = ["computer", "monitor", "keyboard", "mouse", "mouse mat", "hdmi
 chosen_parts = []
 current_choice = ""
 
-while current_choice != 0:
+while current_choice != -1:
     print("Please select the computer part you want to add: \n")
     for number, part in enumerate(computer_parts):
-        print("{0}: {1}".format(number + 1, part))
-    if current_choice in (i for i in range(computer_parts)):
-        print(current_choice)
-    current_choice = int(input())
+        print("{0}: {1}".format(number +1, part))
+    if current_choice in (prt_num for prt_num, prt_name in enumerate(chosen_parts)):
+        print("Removing {}\n".format(current_choice))
+        chosen_parts.remove(computer_parts[current_choice])
+        print("Your current list: {}".format(chosen_parts))
+    elif current_choice in (prt_num for prt_num, prt_name in enumerate(computer_parts)):
+        print("Adding {}\n".format(current_choice))
+        chosen_parts.append(computer_parts[current_choice])
+        print("Your current list: {}".format(chosen_parts))
+    else:
+        print("Please inform a valid number: \n")
+    current_choice = int(input()) - 1
+
+
+print(chosen_parts)
---
+++ ex39_sorting_lists.py
@@ -0,0 +1,15 @@
+# Sorting Lists
+
+numbers = [2, 4, 6, 8, 1, 3, 5, 7, 9]
+another_numbers = numbers
+
+print(numbers)
+numbers.sort()
+print(numbers)
+numbers.sort(reverse=True)
+print(numbers)
+print(another_numbers)
+
+# What happened above? Learn more about mutable and immutable objects here:
+# https://medium.com/@meghamohan/mutable-and-immutable-side-of-python-c2145cf72747
+
